3a51949c43994eb265a800573e913c5eda2d39ea
Endurance Idehen <endurance.idehen@rigup.co>
2020-07-09 00:12:57 -0500

    types are done

--- src/api/pokemon/pokemon.ql.ts
+++ src/api/pokemon/pokemon.ql.ts
@@ -2,9 +2,9 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType('Pokemon')
 export class PokemonQl {
-  @Field(_ => ID)
-  public id: string;
-
-  @Field()
+  @Field({ nullable: true })
   public name: string;
+
+  @Field({ nullable: true })
+  public url: string;
 }
--- src/api/pokemon/pokemon.resolver.ts
+++ src/api/pokemon/pokemon.resolver.ts
@@ -2,19 +2,24 @@ import { Query, Resolver } from '@nestjs/graphql';
 import { PokemonQl } from './pokemon.ql';
 import { Inject } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
-import { pokemon } from '@endurance/grpc-pokemon-server/dist/generated/rpc';
-import PokemonService = pokemon.PokemonService;
+import { PokemonServiceClient } from '@endurance/grpc-pokemon-server';
 
 @Resolver(of => PokemonQl)
 export class PokemonResolver {
-  private readonly _pokemonService: PokemonService;
+  private readonly _pokemonService: PokemonServiceClient;
 
   constructor(@Inject('POKEMON_PACKAGE') private client: ClientGrpc) {
     this._pokemonService = this.client.getService('PokemonService');
   }
 
+  @Query(_ => [PokemonQl])
+  public async pokemon() {
+    const results = await this._pokemonService.findAll({}).toPromise();
+    return results.pokemon;
+  }
+
   @Query(_ => PokemonQl)
-  public pokemon() {
+  public singlePokemon() {
     return this._pokemonService.findOne({ id: 1 });
   }
 }
